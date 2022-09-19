3074addf11c002c8c5490d47428b062d986890ad
Reiner Tupaz <rstupaz@hotmail.com>
2022-09-19 23:15:04 +0800

    Added Initial Models

---
+++ BillTracker/Models/Bill.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using SQLite;
+
+namespace BillTracker.Models
+{
+    [Table("bills")]
+    public class Bill
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int BillId { get; set; }
+
+        // Foreign key to Biller
+        public int BillerId { get; set; }
+
+        public DateTime DateDue { get; set; }
+
+        public double MinAmount { get; set; }
+
+        public double TotalAmount { get; set; }
+
+        public bool IsPaid { get; set; }
+
+        public DateTime DatePaid { get; set; }
+    }
+}
+
---
+++ BillTracker/Models/Biller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SQLite;
+
+namespace BillTracker.Models
+{
+    [Table("billers")]
+    public class Biller
+    {
+        [PrimaryKey, AutoIncrement, Column("_id")]
+        public int BillerId { get; set; }
+
+        [MaxLength(250)]
+        public string BillerName { get; set; }
+
+        [MaxLength(250)]
+        public string BillerReferenceNo { get; set; }
+
+        [MaxLength(250)]
+        public string BillerType { get; set; }
+
+        public DateTime DateAdded { get; set; }
+    }
+}
+
