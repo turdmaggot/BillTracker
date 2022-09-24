8c50a18b1afa46e6a175fde1540bc5128926ce76
Reiner Tupaz <rstupaz@hotmail.com>
2022-09-25 00:16:01 +0800

    Added provisions for adding a bill for a biller.

--- BillTracker/ViewModels/ViewBillerViewModel.cs
+++ BillTracker/ViewModels/ViewBillerViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using BillTracker.Helpers;
+using System.Windows.Input;
 using BillTracker.Models;
 using BillTracker.Services;
 
@@ -10,6 +12,7 @@ namespace BillTracker.ViewModels
         #region Properties
 
         private readonly SQLiteRepository _sqliteRepo;
+        public ObservableCollection<Bill> Bills { get; set; } = new ObservableCollection<Bill>();
 
         private Biller _biller;
         public Biller Biller
@@ -32,7 +35,41 @@ namespace BillTracker.ViewModels
         {
             _sqliteRepo = new SQLiteRepository();
             Biller = biller;
+
+            // TODO: Get Bills from Biller, then put to Bills
         }
+
+        #endregion
+
+        #region Commands
+
+        public ICommand AddBillCommand => new Command(async () =>
+        {
+            // TODO: Add logic to add Bill
+
+
+            await ToastUtility.ShowShortToast("Bill added.");
+        });
+
+        public ICommand SelectedBillCommand => new Command<Bill>(async (bill) =>
+        {
+            string res = await App.Current.MainPage.DisplayActionSheet("Operation", "Cancel", null, "Update", "Delete");
+
+            switch (res)
+            {
+                case "Update":
+                    // TODO
+                    break;
+                case "Delete":
+                    // TODO
+                    //int del = _sqliteRepo.DeleteBill(bill);
+                    //if (del > 0)
+                    //{
+                    //    Bills.Remove(bill);
+                    //}
+                    break;
+            }
+        });
 
         #endregion
     }
